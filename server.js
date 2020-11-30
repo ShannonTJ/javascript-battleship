3f216010da48eacb379be8b25643b39e9059aee1
Shannon TJ <shannontj@shaw.ca>
2020-11-29 17:03:48 -0700

    server running

--- server.js
+++ server.js
@@ -6,3 +6,14 @@ const socketio = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+//set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//Start server
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+//Handle a socket connection request from web client
+io.on("connection", (socket) => {
+  console.log("New WS connection");
+});
